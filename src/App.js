cdc3b56d805c7f4105d8d012e3966e66aab28ed3
Paulia <andersz.paulina@gmail.com>
2017-08-20 18:34:41 +0200

    FlipCard box menu

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FlipCard from 'react-flipcard';
+
+
+const NavigationBar= () => (
+  <div className="row">
+    <div className="navigation-bar-small">Small devices navigation bar</div>
+    <div className="navigation-bar">
+      <div>
+        <span className='navigation-bar-logo'> best<br/>insure.ie</span>
+      </div>
+      <div>
+        <span className='navigation-bar-in-touch'>
+        <span>Home</span>
+        <span>About Us</span>
+        <span>Contact Us</span>
+        </span>
+      </div>
+      <div>
+        <span className='navigation-bar-contact-number'>
+          1890 876 077
+        </span>
+        <span className='naviagtion-bar-quote-button'>
+          <button className='get-quote-now-button'>GET QUOTE NOW</button>
+        </span>  
+      </div>  
+    </div>
+   </div> 
+);
 
 const SingleElementMenu = (props) => (
-   <div className="single-element-menu">
-    <img src={logo} alt="single-element-logo" className="single-element-logo"/>
-    <h4>{props.heading}</h4>
-    
-  </div> 
+  <FlipCard type="vertical" >
+    <div className="single-element-menu-front">
+      <img src={logo} alt="single-element-logo" className="single-element-logo"/>
+      <h4>{props.heading}</h4>
+    </div> 
+    <div className="single-element-menu-back">
+      <h4>GET QUOTE NOW</h4>
+    </div> 
+  </FlipCard>
 );
 
 const DoubleElementMenu = (props)=>(
@@ -17,15 +49,14 @@ const DoubleElementMenu = (props)=>(
   </div> 
 );
 
-
 const BoxMenu = ({Data}) => {
   const count = Data.length/2;
   const list = new Array(count).fill(0).map(
     
     (currentValue, index) => {
     return (
-      <div style={{paddingTop:`${count*2 - index*2}em` }}>
-        <DoubleElementMenu  twoElement={Data.slice(2*index, 2*index+2)}/>
+      <div style={{paddingTop:`${count*2 - index*2}em`}} key={index}>
+        <DoubleElementMenu   twoElement={Data.slice(2*index, 2*index+2)}/>
       </div>
       )
     }
@@ -38,7 +69,6 @@ const BoxMenu = ({Data}) => {
 );
 }
 
-
 const BoxMenuData = [
   { image: logo, heading: 'Heading1' },
   { image: logo, heading: 'Heading2' },
@@ -46,13 +76,16 @@ const BoxMenuData = [
   { image: logo, heading: 'Heading4' },
   { image: logo, heading: 'Heading5' },
   { image: logo, heading: 'Heading6' },
+  { image: logo, heading: 'Heading7' },
+  { image: logo, heading: 'Heading8' }, 
 ]
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-     <BoxMenu  Data={BoxMenuData}/>
+        <NavigationBar />
+        <BoxMenu  Data={BoxMenuData}/>
      </div>
     );
   }
