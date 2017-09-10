df7b90189717b4b7d39c4085e456fd49c8ed3aa5
Paulia <andersz.paulina@gmail.com>
2017-09-10 19:49:36 +0200

    Styles for box menu. RWD medium devices navbar and box menu.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import FlipCard from 'react-flipcard';
 import phone from './images/phone_white.svg';
+import shield from './images/shield_check.svg';
+import truck from './images/truck.svg';
+import heart from './images/heart.svg';
+import home from './images/home.svg';
+import umbrella from './images/umbrella.svg';
+import hand from './images/hand_money.svg';
+import airplane from './images/airplane.svg';
+import commercial from './images/commercial.svg';
+
 
 
 const NavigationBar= () => (
@@ -18,6 +26,13 @@ const NavigationBar= () => (
         <span>About Us</span>
         <span>Contact Us</span>
         </span>
+      </div>
+      <div>
+          <ul className='navigation-bar-in-touch-mid-devices'>
+            <li>Home</li>
+            <li>About Us</li>
+           <li>Contact Us</li>
+          </ul>
       </div>
       <div>
         <span className='navigation-bar-contact-number'>
@@ -37,7 +52,7 @@ const NavigationBar= () => (
 const SingleElementMenu = (props) => (
   <FlipCard type="vertical" >
     <div className="single-element-menu-front">
-      <img src={logo} alt="single-element-logo" className="single-element-logo"/>
+      <img src={props.image} alt="single-element-logo" className="single-element-logo"/>
       <h2>{props.heading}</h2>
     </div> 
     <div className="single-element-menu-back">
@@ -48,43 +63,67 @@ const SingleElementMenu = (props) => (
 
 const DoubleElementMenu = (props)=>(
    <div className="double-element-menu">
-    <SingleElementMenu heading={props.twoElement[0].heading} />
-    <SingleElementMenu heading={props.twoElement[1].heading} />
+    <SingleElementMenu heading={props.twoElement[0].heading}  image={props.twoElement[0].image}/>
+    <SingleElementMenu heading={props.twoElement[1].heading} image={props.twoElement[1].image}/>
   </div> 
 );
 
+const QuadrupleElementMenu = (props)=>(
+   <div className="Quadruple-element-menu">
+    <SingleElementMenu heading={props.fourElement[0].heading}  image={props.fourElement[0].image}/>
+    <SingleElementMenu heading={props.fourElement[1].heading} image={props.fourElement[1].image}/>
+    <SingleElementMenu heading={props.fourElement[2].heading} image={props.fourElement[2].image}/>
+    <SingleElementMenu heading={props.fourElement[3].heading} image={props.fourElement[3].image}/>
+  </div> 
+);
+
+
 const BoxMenu = ({Data}) => {
-  const count = Data.length/2;
-  const list = new Array(count).fill(0).map(
+  const count_desktop = Data.length/2;
+  const list_desktop = new Array(count_desktop).fill(0).map(
     
     (currentValue, index) => {
     return (
-      <div style={{paddingTop:`${count*2 - index*2}em`}} key={index}>
+      <div style={{paddingTop:`${count_desktop*2 - index*2}em`}} key={index}>
         <DoubleElementMenu   twoElement={Data.slice(2*index, 2*index+2)}/>
-      </div>
+     </div>
       )
     }
 );
+  const count_medium = Data.length/4;
+  const list_MediumDevices = new Array(count_medium ).fill(0).map(
+    
+    (currentValue, index) => {
+    return (
+      <div style={{paddingTop:`${count_medium *4 - index*4}em`}} key={index}>
+        <QuadrupleElementMenu    fourElement={Data.slice(4*index, 4*index+4)}/>
+     </div>
+      )
+    }
+);
+
    
   return (
   <div className="row">  
-    <div className="box-menu">
-        {list}
+    <div className="box-menu-desktop">
+        {list_desktop}
+        {list_MediumDevices}
     </div>
   </div>   
 );
 }
 
 const BoxMenuData = [
-  { image: logo, heading: 'Life Assurance' },
-  { image: logo, heading: 'Commercial Cover' },
-  { image: logo, heading: 'Commercial Motor Insurance' },
-  { image: logo, heading: 'Income Protection' },
-  { image: logo, heading: 'Health Insurance' },
-  { image: logo, heading: 'Pension Advice' },
-  { image: logo, heading: 'Home Insurance' },
-  { image: logo, heading: 'Travel Cover' }, 
+  { image: shield, heading: 'Life Assurance' },
+  { image: commercial, heading: 'Commercial Cover' },
+  { image: truck, heading: 'Commercial Motor Insurance' },
+  { image: umbrella, heading: 'Income Protection' },
+  { image: heart, heading: 'Health Insurance' },
+  { image: hand, heading: 'Pension Advice' },
+  { image: home, heading: 'Home Insurance' },
+  { image: airplane, heading: 'Travel Cover' }, 
 ]
+
 
 class App extends Component {
   render() {
